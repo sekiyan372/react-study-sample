dc6182075386c8984cb53d6887352c7d77b29553
sekiyan372 <b1917181@planet.kanazawa-it.ac.jp>
2022-02-15 14:10:59 +0900

    prop-typesのeslintの設定を無効化

--- src/components/week2/ChildCard.tsx
+++ src/components/week2/ChildCard.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/prop-types */
 import React, { VFC } from 'react'
 
 type Props = {
--- src/components/week2/ExChild.tsx
+++ src/components/week2/ExChild.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/prop-types */
 import React, { VFC } from 'react'
 import styled from 'styled-components'
 import FSN from "~/images/fsn.jpg"
