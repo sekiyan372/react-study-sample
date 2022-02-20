290c972c7a77c71cfe46b966fc611123f9a81289
sekiyan372 <b1917181@planet.kanazawa-it.ac.jp>
2022-02-20 20:57:24 +0900

    サンプルプログラムを更新

--- src/pages/week4/Ex.tsx
+++ src/pages/week4/Ex.tsx
@@ -1,11 +1,27 @@
 // Access to http://localhost:3000/week4/ex/
 
-import React, { VFC } from 'react'
+import React, { VFC, useState } from 'react'
 
 const ViewTodos: VFC = () => {
+  // チェックボックスの State
+  const [checked, setChecked] = useState<boolean>(false)
+
   return (
     <>
       <h1>View Todos Page</h1>
+
+      <input
+        type="checkbox"
+        id="completed"
+        checked={checked}
+        onClick={() => setChecked(!checked)}
+      />
+      <label htmlFor="completed">未完了だけを表示</label>
+
+      {/* ここにリスト表示する */}
+      <ul>
+        <li></li>
+      </ul>
     </>
   )
 }
