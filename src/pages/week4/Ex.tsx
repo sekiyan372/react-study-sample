cf727cf9dd51ead70b0760114940959d3df0840a
sekiyan372 <b1917181@planet.kanazawa-it.ac.jp>
2022-02-24 17:06:31 +0900

    week5の演習問題の回答を更新

--- src/pages/week4/Ex.tsx
+++ src/pages/week4/Ex.tsx
@@ -1,10 +1,28 @@
 // Access to http://localhost:3000/week4/ex/
 
-import React, { VFC, useState } from 'react'
+import React, { VFC, useState, useEffect } from 'react'
+
+type Todo = {
+  userId: number
+  id: number
+  title: string
+  completed: boolean
+}
 
 const ViewTodos: VFC = () => {
-  // チェックボックスの State
   const [checked, setChecked] = useState<boolean>(false)
+  const [todos, setTodos] = useState<Todo[]>([])
+
+  useEffect(() => {
+    const fetchPost = async (): Promise<Todo[]> => {
+      const response = await fetch('https://jsonplaceholder.typicode.com/todos')
+      return await response.json()
+    }
+    fetchPost().then((data) => {
+      const newData = checked ? data.filter((todo) => !todo.completed) : data
+      setTodos(newData)
+    })
+  }, [checked])
 
   return (
     <>
@@ -18,9 +36,12 @@ const ViewTodos: VFC = () => {
       />
       <label htmlFor="completed">未完了だけを表示</label>
 
-      {/* ここにリスト表示する */}
       <ul>
-        <li></li>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.title} {todo.completed && '✅'}
+          </li>
+        ))}
       </ul>
     </>
   )
